025c8e32943f33f381d86e98bba7f80a781b801a
mr_hamza1 <itz.mr.hamzi@gmail.com>
2025-09-29 19:01:44 +0500

    deploying

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,19 +1,22 @@
-import { Box, Button } from '@mui/material'
-import { useRouter } from 'next/router'
-import React from 'react'
+"use client";
 
-const page = () => {
+import { Box, Button } from "@mui/material";
+import { useRouter } from "next/navigation";
+import React from "react";
+
+const Page = () => {
   const router = useRouter();
+
   return (
     <Box>
-      <Button onclick={()=> router.push("/signup")}>
-        move on signup page
+      <Button mb={2} variant="contained" onClick={() => router.push("/signup")}>
+        Move to Signup Page
       </Button>
-      <Button onclick={()=> router.push("/pricing")}>
-        move on pricing
+      <Button  variant="contained" color= "red" onClick={() => router.push("/pricing")}>
+        Move to Pricing
       </Button>
     </Box>
-  )
-}
+  );
+};
 
-export default page
+export default Page;
